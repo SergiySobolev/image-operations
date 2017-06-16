5ed618eb77cdecbab1a192afa4ff9604d2ffd8bf
Serhii Soboliev <sergiy.sobolev@gmail.com>
2017-06-17 01:54:29 +0300

    convolution viewer base

---
+++ convolution/ConvolutionViewer.m
@@ -0,0 +1,56 @@
+function varargout = ConvolutionViewer(varargin)
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @ConvolutionViewer_OpeningFcn, ...
+                   'gui_OutputFcn',  @ConvolutionViewer_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+
+function ConvolutionViewer_OpeningFcn(hObject, eventdata, handles, varargin)
+    handles.output = hObject;
+    guidata(hObject, handles);
+
+
+function varargout = ConvolutionViewer_OutputFcn(hObject, eventdata, handles) 
+    varargout{1} = handles.output;
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+    Y = str2num(get(handles.edit1, 'String'));
+    sz = size(Y,2);
+    X = 1:1:sz;
+    stem(X,Y);
